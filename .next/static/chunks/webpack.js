6baea464a9ce71acf106261a9e5e887758f4e57a
Mohammed Salah <mohammed_salah@icloud.com>
2022-02-17 18:52:21 +0000

    projects section set up

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0e3f7873f1fe1d94ec74"; }
+/******/ 		__webpack_require__.h = function() { return "58e290d26254587c1928"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,11 +3,39 @@ import React from 'react';
 import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
-
+const projectsDemo = [{
+  title:'Project 1',
+  description:'This is a project description'
+},{
+  title:'Project 2',
+  description:'This is a project description'
+},{
+  title:'Project 3',
+  description:'This is a project description'
+},{
+  title:'Project 4',
+  description:'This is a project description'
+},{
+  title:'Project 5',
+  description:'This is a project description'
+},{
+  title:'Project 6',
+  description:'This is a project description'
+}]
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section nopadding id="projects">
+    <SectionDivider/>
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+      {projectsDemo.map((project) => (
+        <div>
+          {project.title}
+          <br/>
+          {project.description}
+        </div>
+      ))}
+    </GridContainer>
+  </Section>
 );
 
 export default Projects;
